d28f9459ca11a6fb41285db5e8d21610c790731b
erecik <eret112@gmail.com>
2019-08-26 17:31:05 +0200

    1.454 dont really know what i have added now

--- gui.py
+++ gui.py
@@ -1,6 +1,6 @@
 from PyQt5 import QtWidgets, uic
 from PyQt5.QtWidgets import *
-import bypass
+import sys
 
 def przypisz():
     if dlg.loginLog.text()  =="": # or dlg.password.text().isEmpty() or dlg.url.text().isEmpty() or dlg.world.text().isEmpty():
@@ -17,14 +17,29 @@ def przypisz():
 def error_puste_pole():
     QMessageBox.information(None, "404", "One or more field is empty")
 
-def timeline_add(): # poprawic
-    pass
+def timeline_add_train(): # poprawic
+    slot = f"{root.time_train.text()} | TRAIN | ID: {root.ID_village_train.text()} | {root.train_type.currentText()} | {root.amount_train.text()}"
+
+    root.timeline.addItem(slot)
+
+def timeline_add_build():  # poprawic
+    slot = f"{root.time_build.text()} | BUILD | ID: {root.ID_village_build.text()} | {root.build_type.currentText()}"
+
+    root.timeline.addItem(slot)
+
+    slot_raw = root.time_build.text().split(":")
+    print(slot_raw)
+    slot_raw_raw = list(map(int,slot_raw))
+    print(slot_raw_raw)
+
+
 
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication([])
     dlg = uic.loadUi("main_root.ui")
     root = uic.loadUi("main_win.ui")
+
 
     login = ""
     password = ""
@@ -33,11 +48,11 @@ if __name__ == '__main__':
 
 
     dlg.login_button.clicked.connect(przypisz)
-    root.addToTime.clicked.connect(timeline_add)
-
+    root.addToTime_train.clicked.connect(timeline_add_train)
+    root.addToTime_build.clicked.connect(timeline_add_build)
 
 
 
     dlg.show()
-    app.exec()
+    sys.exit(app.exec())
 
